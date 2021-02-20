ffa181b7112e5eeeb9362d2e09ab44a0b296b93d
Steve Wilson <steve.wilson@digtial.justice.gov.uk>
2021-02-20 14:00:00 +0000

    Inital commit
    - basic express app
    - index route
    - sass, govkit, node-sass-middleware & nunjucks deps installed
    - config to load govkit
    - config to build sass

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+module.exports =  router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
